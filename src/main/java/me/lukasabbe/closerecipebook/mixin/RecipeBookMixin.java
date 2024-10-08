8555376fb04b585290ea724b51c9058c0cec7673
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-10-08 18:49:31 +0200

    Fixed mod after recipe book reworks

--- src/main/java/me/lukasabbe/closerecipebook/mixin/RecipeBookMixin.java
+++ src/main/java/me/lukasabbe/closerecipebook/mixin/RecipeBookMixin.java
@@ -2,11 +2,12 @@ package me.lukasabbe.closerecipebook.mixin;
 
 import me.lukasabbe.closerecipebook.util.RecipeBookUtil;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.recipebook.RecipeBookProvider;
+import net.minecraft.client.gui.screen.ingame.RecipeBookScreen;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.recipebook.ClientRecipeBook;
-import net.minecraft.recipe.book.RecipeBookCategory;
+import net.minecraft.recipe.book.RecipeBookType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -25,8 +26,9 @@ public class RecipeBookMixin {
     @Inject(method = "closeScreen", at= @At("HEAD"))
     public void onClosedInject(CallbackInfo ci){
         if(client != null && client.currentScreen != null){
-            if(client.currentScreen instanceof RecipeBookProvider recipeBookProvider){
-                final RecipeBookCategory category = ((RecipeBookAccessor) recipeBookProvider.getRecipeBookWidget()).getCraftingHandler().getCategory();
+            if(client.currentScreen instanceof RecipeBookScreen<?> recipeBookProvider){
+                final RecipeBookWidget<?> recipeBookWidget = ((RecipeBookScreenAccessor) recipeBookProvider).getRecipeBook();
+                final RecipeBookType category = ((RecipeBookAccessor) recipeBookWidget).getCraftingHandler().getCategory();
                 RecipeBookUtil.closeRecipeBook(recipeBook,networkHandler, category);
             }
         }
--- src/main/java/me/lukasabbe/closerecipebook/util/RecipeBookUtil.java
+++ src/main/java/me/lukasabbe/closerecipebook/util/RecipeBookUtil.java
@@ -4,9 +4,10 @@ import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.recipebook.ClientRecipeBook;
 import net.minecraft.network.packet.c2s.play.RecipeCategoryOptionsC2SPacket;
 import net.minecraft.recipe.book.RecipeBookCategory;
+import net.minecraft.recipe.book.RecipeBookType;
 
 public class RecipeBookUtil {
-    public static void closeRecipeBook(ClientRecipeBook recipeBook, ClientPlayNetworkHandler networkHandler, RecipeBookCategory category){
+    public static void closeRecipeBook(ClientRecipeBook recipeBook, ClientPlayNetworkHandler networkHandler, RecipeBookType category){
         recipeBook.setGuiOpen(category, false);
         networkHandler.sendPacket(new RecipeCategoryOptionsC2SPacket(category,false,false));
     }
